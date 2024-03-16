d39cb30309fe66254ad774d69364f0a8075ae7f5
Wagner Lopes <161225187+Lopeswaprojetos@users.noreply.github.com>
2024-03-16 16:13:55 -0300

    Update convert.js

--- conversao-temperatura/src/test/convert.js
+++ conversao-temperatura/src/test/convert.js
@@ -1,7 +1,7 @@
 const convert = require('../convert')
 var expect  = require("chai").expect;
 
-describe('Conversor de Temperatura', () => {
+describe('Conversor de Temperatur', () => {
 
     it('Deve converter corretamente Fahrenheit para Celsius', (done) => {
 
